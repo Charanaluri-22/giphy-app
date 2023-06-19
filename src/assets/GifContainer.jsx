d612abbb4fd6eac07575b2f946e228dc73fa2925
charan <aluricharan1922@gmail.com>
2023-06-19 23:06:27 +0530

    deployment ready'

--- src/assets/GifContainer.jsx
+++ src/assets/GifContainer.jsx
@@ -1,18 +1,24 @@
 import  { useState } from 'react';
 import "./App.css"
+import API_KEY  from './config';
+import SwalModal from 'sweetalert2';
+
 
 const GifContainer = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [limitTerm,setLimitTerm] = useState('');
   const [gifs, setGifs] = useState([]);
 
   const searchGIFs = () => {
-    const apiKey = import.meta.env.VITE_APIKEY;
-
+    const apiKey = API_KEY
+    if(limitTerm && searchTerm)
+    {
+      
     // Clear previous GIFs
     setGifs([]);
 
     // Make API request
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=10`)
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=${limitTerm}`)
       .then(response => response.json())
       .then(data => {
         const processedGifs = data.data.map(gif => ({
@@ -24,26 +30,62 @@ const GifContainer = () => {
       .catch(error => {
         console.error('Error:', error);
       });
+    }
+    else{
+      SwalModal.fire({
+        title: "Error",
+        text: "please enter both search and limit term",
+        icon: "error",
+        confirmButtonText: "OK"
+      });
+    }
   };
-
+  const deleteGIFS = ()=>
+  {
+    setSearchTerm("");
+    setLimitTerm("");
+    setGifs([]);
+  }
   return (
     <div className="container">
-      <h1 className="heading">GIPHY GIF Search</h1>
-      <input
-        type="text"
-        placeholder="Enter a word"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
+  <div className='formcontainer'>
+    <h1 className="heading">GIPHY GIF Search</h1>
+
+    <label htmlFor="searchInput"><h3>Search Term:</h3></label>
+    <input
+      type="text"
+      id="searchInput"
+      required
+      placeholder="Enter a word"
+      value={searchTerm}
+      onChange={e => setSearchTerm(e.target.value)}
+    /><br/>
+    <label htmlFor="limitInput"><h3>Limit term:</h3></label>
+    <input
+      type="text"
+      id="limitInput"
+      required
+      placeholder="Enter no of gifs you want"
+      value={limitTerm}
+      onChange={e => setLimitTerm(e.target.value)}
+    /><br/>
+
+
+    <div className='inputcontainer'>
       <button onClick={searchGIFs} className="btn">Search</button>
-      <div className="gif-container">
-        {gifs.map((gif, index) => (
-          <div className="gif-item" key={index}>
-            <img src={gif.gifUrl} alt={gif.gifTitle} />
-          </div>
-        ))}
-      </div>
+      <button onClick={deleteGIFS} className="btn">Delete Search</button>
     </div>
+  </div>
+
+  <div className="gif-container">
+    {gifs.map((gif, index) => (
+      <div className="gif-item" key={index}>
+        <img src={gif.gifUrl} alt={gif.gifTitle} />
+      </div>
+    ))}
+  </div>
+</div>
+
   );
 };
 
---
+++ src/assets/config.js
@@ -0,0 +1,2 @@
+const API_KEY = 'QHXMM6HdEr9asGfcMO4eqcqIrgeCoydA'
+export default API_KEY
